7fba0e31991ef13baa0ad7dfa8ad01a2ee349116
poke-hiroki-nakayama <hiroki.nakayama@pokelabo.co.jp>
2015-01-04 00:15:46 +0900

    redisテストにZSETを追加

--- redis_test/test.js
+++ redis_test/test.js
@@ -21,3 +21,22 @@ client.set("test", "うひょー");
 client.get("test", function(err, data){
     console.log(data);
 });
+
+var args = [ 'myzset', 2, 'two', 3, 'three', 1, 'one' , 99, 'ninety-nine' ];
+client.zadd( args, function (err, response) {
+    if (err) throw err;
+    console.log('added ' + response + ' items.');
+});
+
+var args1 = [ 'myzset', 0, 10, 'WITHSCORES' ];
+client.zrange(args1, function (err, response) {
+    if (err) throw err;
+    console.log('example1', response);
+});
+
+var max = 3, min= 1, offset = 1, count = 2;
+var args2 = [ 'myzset', max, min, 'WITHSCORES', 'LIMIT', offset, count ];
+client.zrevrangebyscore(args2, function (err, response) {
+    if (err) throw err;
+    console.log('example2', response);
+});
